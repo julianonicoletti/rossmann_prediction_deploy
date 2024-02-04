d3a059be75c3cdd5938ce2b2e1fe6c03ab4922c6
julianonicoletti <juliano.nicoletti@edu.unifil.br>
2024-02-04 17:35:11 -0300

    change in handler.py, rossmann_predict()

--- handler.py
+++ handler.py
@@ -19,17 +19,19 @@ def rossmann_predict():
             test_raw = pd.DataFrame( test_json, index=[0] )
         else: # multiple example
             test_raw = pd.DataFrame( test_json, columns=test_json[0].keys() )
-            # Instantiate Rossmann class
-            pipeline = Rossmann()
-            # data cleaning
-            df1 = pipeline.data_cleaning( test_raw )
-            # feature engineering
-            df2 = pipeline.feature_engineering( df1 )
-            # data preparation
-            df3 = pipeline.data_preparation( df2 )
-            # prediction
-            df_response = pipeline.get_prediction( model, test_raw, df3 )
-            return df_response
+            
+        # Instantiate Rossmann class
+        pipeline = Rossmann()
+        # data cleaning
+        df1 = pipeline.data_cleaning( test_raw )
+        # feature engineering
+        df2 = pipeline.feature_engineering( df1 )
+        # data preparation
+        df3 = pipeline.data_preparation( df2 )
+        # prediction
+        df_response = pipeline.get_prediction( model, test_raw, df3 )
+        return df_response
+    
     else:
         return Response( '{}', status=200, mimetype='application/json' )
     
